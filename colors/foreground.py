296967d7d7b5ee45bede1bf7feca1197db5005fa
xiaoxTM <gilyou.public@gmail.com>
2018-04-14 12:08:11 +0900

    Create foreground.py

---
+++ colors/foreground.py
@@ -0,0 +1,51 @@
+"""
+    Copyright (C) 2018  Renwu Gao
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from sys import platform
+
+black = '\033[30m'
+red = '\033[31m'
+green = '\033[32m'
+orange = '\033[33m'
+blue = '\033[34m'
+purple = '\033[35m'
+cyan = '\033[36m'
+lightgray = '\033[37m'
+darkgray = '\033[90m'
+lightred = '\033[91m'
+lightgreen = '\033[92m'
+yellow = '\033[93m'
+lightblue = '\033[94m'
+pink = '\033[95m'
+lightcyan = '\033[96m'
+
+if platform.startswith('win'):
+    black = ''
+    red = ''
+    green = ''
+    orange = ''
+    blue = ''
+    purple = ''
+    cyan = ''
+    lightgray = ''
+    darkgray = ''
+    lightred = ''
+    lightgreen = ''
+    yellow = ''
+    lightblue = ''
+    pink = ''
+    lightcyan = ''
